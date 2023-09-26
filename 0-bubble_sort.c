1d8c7ca08901337ef7d7a5dd2320c8839b56b43b
root <root@DESKTOP-DMRT987>
2023-09-26 21:29:55 +0300

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,10 +7,11 @@
  */
 void swap_ints(int *e_m, int *b_k)
 {
-    int tmp;
-    tmp = *e_m;
-    *e_m = *b_k;
-    *b_k = tmp;
+	int tmp;
+
+	tmp = *e_m;
+	*e_m = *b_k;
+	*b_k = tmp;
 }
 
 /**
@@ -21,24 +22,24 @@ void swap_ints(int *e_m, int *b_k)
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t l, len = size;
-    bool bubbly = false;
+	size_t l, len = size;
+	bool bubbly = false;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    while (bubbly == false)
-    {
-        bubbly = true;
-        for (l = 0; l < len - 1; l++)
-        {
-            if (array[l] > array[l + 1])
-            {
-                swap_ints(array + l, array + l + 1);
-                print_array(array, size);
-                bubbly = false;
-            }
-        }
-        len--;
-    }
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (l = 0; l < len - 1; l++)
+		{
+			if (array[l] > array[l + 1])
+			{
+				swap_ints(array + l, array + l + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
 }
